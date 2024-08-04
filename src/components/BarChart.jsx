2394fda73fa0c44007a2854ac1e3f4c61eab73f7
amanshridev <mailtoamanshri@proton.me>
2024-08-04 16:30:57 +0530

    remove chart js and added vectory chart

---
+++ src/components/BarChart.jsx
@@ -0,0 +1,24 @@
+// ./components/BarChart.js
+import React from "react";
+import Chart from "chart.js";
+import { Bar } from "react-Chart.js-2";
+const BarChart = () => {
+  const labels = ["January", "February", "March", "April", "May", "June"];
+  const data = {
+    labels: labels,
+    datasets: [
+      {
+        label: "My First dataset",
+        backgroundColor: "rgb(255, 99, 132)",
+        borderColor: "rgb(255, 99, 132)",
+        data: [0, 10, 5, 2, 20, 30, 45],
+      },
+    ],
+  };
+  return (
+    <div>
+      <Bar data={data} />
+    </div>
+  );
+};
+export default BarChart;
--- src/components/HeroSection/index.jsx
+++ src/components/HeroSection/index.jsx
@@ -1,8 +1,33 @@
+'use client'
+import { useState } from "react";
 import Image from "next/image";
-import { homeHeaderData } from "@/data";
+import { homeHeaderData, dataChart } from "@/data";
 import "./index.css";
+import { CategoryScale } from "chart.js";
+import { BarChart } from "../BarChart";
+
+Chart.register(CategoryScale);
 
 export default function HeroSection() {
+  const [chartData, setChartData] = useState({
+    labels: dataChart.map((data) => data.year), 
+    datasets: [
+      {
+        label: "Users Gained ",
+        data: dataChart.map((data) => data.userGain),
+        backgroundColor: [
+          "rgba(75,192,192,1)",
+          "#ecf0f1",
+          "#50AF95",
+          "#f3ba2f",
+          "#2a71d0"
+        ],
+        borderColor: "black",
+        borderWidth: 2
+      }
+    ]
+  });
+
   return (
     <section className="hero-section">
       {homeHeaderData.map((item) => (
@@ -13,7 +38,9 @@ export default function HeroSection() {
       ))}
       <div>
         <Image src="" width={500} height={500} alt="hero" />
-        <div></div>
+        <div style="width: 800px;">
+          <BarChart chartData={chartData} />
+        </div>
         <div></div>
         <div></div>
       </div>
